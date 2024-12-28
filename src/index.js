9e5db51bcae78df812b1880a5d77b99bb1819f91
Santosh Kumar Pottumuthu <santoshkp@Santoshs-MacBook-Pro.local>
2024-12-28 13:33:15 -0500

    deployment issues

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Import HashRouter instead of BrowserRouter
 import { HashRouter as Router } from 'react-router-dom';
 
+// Create the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap the App component in Router */}
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Log performance metrics if needed
 reportWebVitals();
